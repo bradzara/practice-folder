1e0d2486a0482c048d6e99ef960fb4ba1f8c1cbf
bradzara <bradzara93@gmail.com>
2024-05-07 11:32:55 -0700

    Create items using hashes with symbols

--- store-item.rb
+++ store-item.rb
@@ -1,1 +1,37 @@
 puts "This is a print statment."
+
+item1 = {
+  product: "Apple",
+  department: "Produce",
+  price: "3.99/lb",
+}
+
+item2 = {
+  product: "Steak",
+  department: "Meat",
+  price: "12.99/lb"
+}
+
+item3 = {
+  product: "Penne pasta",
+  department: "Grocery"
+  price: "2.99"
+}
+
+item1 = {
+  :product => "Apple",
+  :department => "Produce",
+  :price => "3.99/lb",
+}
+
+item2 = {
+  :product => "Steak",
+  :department => "Meat",
+  :price => "12.99/lb"
+}
+
+item3 = {
+  :product => "Penne pasta",
+  :department => "Grocery"
+  :price => "2.99"
+}
